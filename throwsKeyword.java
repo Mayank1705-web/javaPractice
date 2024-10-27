3d78528eb06ae83b2d53019d809ea698acfce5d4
Mayank Ingole <mayankingole17may@gmail.com>
2024-10-27 15:41:03 +0530

    throwsKeyword.java

--- throwsKeyword.java
+++ throwsKeyword.java
@@ -1,3 +1,22 @@
 public class throwsKeyword {
-    
+    void div (int a, int b) throws ArithmeticException{
+        int c; 
+        if (b == 1) {
+            throw new ArithmeticException("/by 1");
+        }
+        else{
+            c = a/b;
+            System.out.println (c);
+        }
+    }
+    public static void main(String[] args) {
+        throwsKeyword t1 = new throwsKeyword();
+        try {
+            t1.div(10, 1);
+            t1.div(10, 0);
+        } catch (ArithmeticException e) {
+            System.out.println(e);
+        }
+        System.out.println("Bye");
+    }
 }
