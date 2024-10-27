73d70cdca7b0b13cdfb092ec579ee1fc3c4d5ecb
Mayank Ingole <mayankingole17may@gmail.com>
2024-10-27 17:06:29 +0530

    Add files via upload
    This folder contains all the examples about Threads

---
+++ Threads-20241027T112021Z-001/Threads/Synch1.java
@@ -0,0 +1,57 @@
+class Printer
+{
+	void print(String firstName,String  lastName) 
+	{
+		System.out.print("[" + firstName);
+		try 
+		{
+			Thread.sleep(5000);
+		} 
+		catch(InterruptedException e) 
+		{
+			System.out.println("Interrupted");
+		}
+		System.out.println(" "+lastName+"]");
+	}
+}
+class Caller implements Runnable 
+{
+	String firstName,lastName;
+	Printer target;
+	Thread t;
+	public Caller(Printer target, String firstName,String lastName) 
+	{
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.target = target;
+		t = new Thread(this);
+		t.start();
+	}
+	public void run() 
+	{
+		synchronized(target)
+		{
+			target.print(firstName,lastName);
+		}
+	}
+}
+class Synch1
+{
+	public static void main(String args[]) 
+	{
+		Printer target = new Printer();
+		Caller ob1 = new Caller(target, "Pankaj","Wadhwani");
+		Caller ob2 = new Caller(target, "Programming","World");
+		Caller ob3 = new Caller(target, "Hello","Indore");
+		try 
+		{
+			ob1.t.join();
+			ob2.t.join();
+			ob3.t.join();
+		} 
+		catch(InterruptedException e) 
+		{
+			System.out.println("Interrupted");
+		}
+	}
+}
